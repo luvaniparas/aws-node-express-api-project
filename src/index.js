17cd8fef5d8e68c15f4533c8f42694c23489d005
Paras Luvani <luvaniparas5@gmail.com>
2024-03-04 12:25:09 +0530

    [main] :: serverless setup with env

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+    env: `${process.env.PORT}`
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// server-full app
+// app.listen(3000,() => {
+//   console.log(`app running at http://localhost:3000`);
+// })
+
+
+module.exports.handler = serverless(app);
